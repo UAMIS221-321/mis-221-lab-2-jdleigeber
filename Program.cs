fa9b8b7f62b816a0d69689731fb7ed8ce91d09b8
Jacob Leigeber <jdleigeber@crmson.ua.edu>
2024-09-06 17:42:59 -0500

    Lab2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double costOfSandwich = 4.75;
+double costOfToppings = 0.55;
+double discountRate = 0.10;
+
+Console.WriteLine("Enter the number of sandwiches");
+
+int numberOfSandwiches = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the number of toppings");
+
+int numberOfToppings = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter a tip amount");
+
+double tipAmount = double.Parse(Console.ReadLine());
+
+double totalSandwichCost = costOfSandwich * numberOfSandwiches;
+
+double totalToppingCost = costOfToppings * numberOfToppings;
+
+double baseCost = totalSandwichCost + totalToppingCost;
+
+double orderCost = tipAmount + baseCost * (1-discountRate);
+
+orderCost = Math.Round(orderCost,2);
+
+Console.WriteLine("Your order cost is " + "$" + orderCost);
+
